b81f73db30acccb05d21dd05b66d2fdcdee93228
interactafraz <interact@afraz.de>
2023-05-14 20:56:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# anystate
+# AnyState
 AnyState is a lightweight tool to easily store and retrieve values (like texts, numbers or arrays) in a JSON file using your own web server.
+
+It was developed for automated processes where different devices, apps and operating systems need to exchange data with each other in a simple and fast way. This includes workflow tools like [n8n](https://github.com/n8n-io/n8n) as well as iOS Shortcuts.
+
+### Possible use cases
+* Checkin at work using iOS shortcut, use AnyState to set `working` status to `true`, add timestamp to Google Sheet via n8n
+* Get cards from Trello board via n8n, store their attributes with AnyState, use a custom dashboard in your office to display Trello board statistics
+
+## Features
+* Save keys and values (via POST method)
+* Get single value for specific key (via GET parameter)
+* Get timestamp for specific key (via GET parameter)
+* Limit maximum storage time for keys (allows keys to expire)
+* Preview keys and values in browser
+
+## Prerequisites
+* Common web server with PHP support
+
+## Installation
+* Copy files to web server
+
+## Usage
+### Save data
+The script expects POST data in the following format
+
+    {"currentTemp": "26","currentConditions": "rainy"}
+
+### Read data
+Apart from reading the whole JSON file you can also use parameters (one at a time) to get the data for specific keys. If you omit these parameters the script will output a plain preview of all keys and values (for better readability this doesn't output arrays).
+
+* Get value of key with `index.php?state=currentTemp` (arrays not supported)
+* Get last update timestamp for individual value `index.php?time=currentTemp` (arrays not supported)
+
+## License
+tba
---
+++ index.php
@@ -0,0 +1,169 @@
+<?php 
+
+$file = "states.json";
+$timeLimit = 365; //How many days a state should be stored
+$previewTimeZone = "Europe/Berlin";//Only for preview output
+
+$stateData = array();
+function addToStates($sentData,$fp) {
+	global $stateData;
+	global $timeLimit;
+	
+	$timestamp = time();	
+	
+	if( count($stateData) > 0 ){ //If any entry exists
+		foreach ($sentData as $sentKey => $sentValue) {
+			$entryFound = false;
+			$entryExistingCounter = 0;
+			
+			foreach ($stateData as $entryExisting) { //Check existing entries
+				if ($entryFound == false){ //If target entry not found (yet)
+					if( !empty($entryExisting[$sentKey]) ){ //If entry for sent key has a value
+						$entryFound = true;
+						
+						$stateData[$entryExistingCounter]['time'] = $timestamp; //Override
+						$stateData[$entryExistingCounter][$sentKey] = $sentValue; //Override
+						
+						break; //Stop searching in existing entries
+					}
+				}
+				
+				$entryExistingCounter = $entryExistingCounter + 1;
+			}
+			
+			if ($entryFound == false){ //If entry not found at all
+				$entry = array();
+				$entry['time'] = $timestamp;
+				$entry[$sentKey] = $sentValue;
+				array_push($stateData, $entry); //Add to existing entries	
+			}
+		}
+	}
+	else{ //No entry exists
+		foreach ($sentData as $key => $value) {
+			$entry = array();
+			$entry['time'] = $timestamp;
+			$entry[$key] = $value;
+			array_push($stateData, $entry); //Add to existing entries			
+		}
+	}
+	
+	usort($stateData, function($a, $b) {
+		return $b['time'] - $a['time'];
+	});
+	
+	$stateData = array_values($stateData); //Rebuild index
+	
+	rewind($fp);
+	ftruncate($fp, 0);
+	fwrite($fp, json_encode($stateData)); //Save new data to file
+}
+
+if ( file_exists($file) ) { //Check if data exists
+	
+	$fp = fopen($file, 'r+');
+	flock($fp, LOCK_EX); //Lock file to avoid other processes writing to it simlutanously 
+
+	$jsonData = stream_get_contents($fp);
+	if($jsonData == ""){
+		echo "Could not read Log file (maybe empty)";
+		die;
+	}
+	$stateData = json_decode($jsonData, true);
+	
+	$entryExistingCounter = 0;
+	
+	foreach ($stateData as $entryExisting) {
+		if ($entryExisting['time'] < date("U",strtotime("-".$timeLimit." days")) ){//If older than limit
+			unset($stateData[$entryExistingCounter]);
+		}
+		$entryExistingCounter = $entryExistingCounter + 1;
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty(json_decode($_POST['data'], true))) {
+		//Debug
+		//$debug = fopen('debug.txt', 'w');
+		//fwrite($debug, $_POST); //Save new data to file
+		//fclose($debug);
+		//Debug
+		addToStates( json_decode($_POST['data'], true),$fp );
+	}
+	
+	flock($fp, LOCK_UN); //Unlock file for further access
+	fclose($fp);
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AnyState</title>
+	</head>
+	<body>
+		<?php 
+			if ( file_exists($file) ) { //Check if data exists
+				
+				if (!empty($_GET)) { 
+					if ( isset($_GET['state']) ){ //If specific value(s) requested
+						foreach ($stateData as $entryExisting) { //Get every entry
+							foreach ($entryExisting as $key => $value) { //Get key and timestamp values
+								if ($_GET['state'] == $key){ //If target key has been found
+									echo $value;
+									break 2;
+								}
+							}
+						}
+					}
+					elseif ( isset($_GET['time']) ){ //If time value for specific key requested
+						foreach ($stateData as $entryExisting) { //Get every entry
+							foreach ($entryExisting as $key => $value) { //Get key and timestamp values
+								if ($_GET['time'] == $key){ //If target key has been found
+									echo $entryExisting['time'];
+									break 2;
+								}
+							}
+							
+						}
+					}
+						
+				}
+				else {
+					foreach ($stateData as $index => $entry) {
+						$elementCount = count($entry);
+						$counter = 0;
+						
+						foreach ($entry as $key => $value) {
+							$counter = $counter + 1;
+							
+							if($key == 'time'){
+								$time = DateTime::createFromFormat('U', $value)->setTimezone(new DateTimeZone($previewTimeZone))->format('d.m.Y \a\t H:i'); //Convert to datetime object							
+								echo $time . ' - ';
+							}
+							else{ //Value
+								if ( is_array($value) ){ //If value is array
+										echo $key . ' = ' . 'Array';									
+								}
+								else{
+									echo $key . ' = ' . $value;
+								}
+							}
+							
+							
+							if( $counter != $elementCount && $key != 'time'){
+								echo ' | ';
+							}
+							
+						}
+						
+						echo "<br>";
+					}	
+				}
+			
+				
+			}
+		?>
+	</body>
+</html>
